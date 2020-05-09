a9949a093afe4e5bcdb398d6c4c777fdc568deab
Flaque <evanjamesconrad@gmail.com>
2020-05-09 09:51:12 -0700

    WOOO

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Hello Gonzaga!"));
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
